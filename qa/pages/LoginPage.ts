cdf337702b27e46552b4651fe12bb66b7abf13b1
testingsdet2025 <testing.sdet2025@gmail.com>
2026-05-05 23:36:11 +0530

    Update LoginPage.ts

--- qa/pages/LoginPage.ts
+++ qa/pages/LoginPage.ts
@@ -1,1 +1,15 @@
+import { Page } from '@playwright/test';
 
+export class LoginPage {
+  constructor(private page: Page) {}
+
+  async navigate() {
+    await this.page.goto('/login');
+  }
+
+  async login(username: string, password: string) {
+    await this.page.fill('#username', username);
+    await this.page.fill('#password', password);
+    await this.page.click('#loginBtn');
+  }
+}
