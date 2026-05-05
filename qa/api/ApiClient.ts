d67b1e2efd373a6c7794f7a133e3820a74e5857d
testingsdet2025 <testing.sdet2025@gmail.com>
2026-05-05 23:40:14 +0530

    Update ApiClient.ts

--- qa/api/ApiClient.ts
+++ qa/api/ApiClient.ts
@@ -1,1 +1,13 @@
+import { request } from '@playwright/test';
 
+export class ApiClient {
+  async get(endpoint: string) {
+    const context = await request.newContext();
+    return context.get(endpoint);
+  }
+
+  async post(endpoint: string, data: any) {
+    const context = await request.newContext();
+    return context.post(endpoint, { data });
+  }
+}
