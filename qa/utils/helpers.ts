86c82788c809e0bbcd2f29cc0e5d8d4b505f9a22
testingsdet2025 <testing.sdet2025@gmail.com>
2026-05-05 23:56:37 +0530

    Update helpers.ts

--- qa/utils/helpers.ts
+++ qa/utils/helpers.ts
@@ -1,1 +1,8 @@
+import * as fs from 'fs';
+import * as path from 'path';
 
+export function readTestData(fileName: string) {
+  const filePath = path.resolve(__dirname, `../test-data/${fileName}`);
+  const data = fs.readFileSync(filePath, 'utf-8');
+  return JSON.parse(data);
+}
