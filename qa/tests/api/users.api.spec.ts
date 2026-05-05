bd1102c1637124085160a6ec6d1b29cab54d9b98
testingsdet2025 <testing.sdet2025@gmail.com>
2026-05-05 23:20:15 +0530

    Update users.api.spec.ts

--- qa/tests/api/users.api.spec.ts
+++ qa/tests/api/users.api.spec.ts
@@ -1,1 +1,13 @@
+import { test, expect } from '@playwright/test';
+import { ApiClient } from '../../api/ApiClient';
+import { ENDPOINTS } from '../../api/endpoints';
 
+test('Get users API', async () => {
+  const api = new ApiClient();
+
+  const response = await api.get(ENDPOINTS.USERS);
+  expect(response.status()).toBe(200);
+
+  const body = await response.json();
+  expect(body).toHaveProperty('data');
+});
