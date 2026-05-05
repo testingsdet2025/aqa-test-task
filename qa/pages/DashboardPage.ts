13a3cd7a84974bdf8780f0795befb005691a775b
testingsdet2025 <testing.sdet2025@gmail.com>
2026-05-05 23:38:23 +0530

    Update DashboardPage.ts

--- qa/pages/DashboardPage.ts
+++ qa/pages/DashboardPage.ts
@@ -1,1 +1,26 @@
+import { Page, Locator } from '@playwright/test';
 
+export class DashboardPage {
+  readonly page: Page;
+  readonly dashboardHeader: Locator;
+  readonly profileIcon: Locator;
+  readonly settingsLink: Locator;
+  readonly logoutButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.dashboardHeader = page.locator('h1');
+    this.profileIcon = page.locator('#profileIcon');
+    this.settingsLink = page.locator('#settingsLink');
+    this.logoutButton = page.locator('#logoutBtn');
+  }
+
+  async goToSettings() {
+    await this.settingsLink.click();
+  }
+
+  async logout() {
+    await this.profileIcon.click();
+    await this.logoutButton.click();
+  }
+}
