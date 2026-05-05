718edfd1b2dfe780f3638d37948ed7bfb50ccd54
testingsdet2025 <testing.sdet2025@gmail.com>
2026-05-05 23:39:24 +0530

    Update testFixture.ts

--- qa/fixtures/testFixture.ts
+++ qa/fixtures/testFixture.ts
@@ -1,1 +1,9 @@
+import { test as base } from '@playwright/test';
+import { ApiClient } from '../api/ApiClient';
 
+export const test = base.extend({
+  api: async ({}, use) => {
+    const api = new ApiClient();
+    await use(api);
+  }
+});
